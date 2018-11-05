1697b9e45209182dadc05c2e6bd48b5a001da1fb
shastel <max@shas.tel>
2018-11-05 15:26:37 +0300

    Allow more than 1 req at time

--- index.js
+++ index.js
@@ -6,69 +6,84 @@ const ProgressBar = require('progress');
 const commandLineArgs = require('command-line-args');
 const octokit = require('@octokit/rest')();
 
-const { star, pattern, secret } = commandLineArgs([
-  { name: 'star', type: Boolean, },
-  { name: 'pattern', type: String, defaultOption: '2018Q3' },
-  { name: 'secret', type: String, defaultOption: process.env.GITHUB_OAUTH_KEY }
+let { secret, pattern, star } = commandLineArgs([
+  { name: 'pattern', type: String, alias: 'p', defaultOption: '2018Q3' },
+  { name: 'secret', type: String, alias: 's' },
+  { name: 'star', type: Boolean, alias: 't' },
 ]);
 
-if (!secret) {
+if (!secret || !process.env.GITHUB_OAUTH_KEY) {
+  // eslint-disable-next-line
   console.error('Param \'secret\' is missing');
   process.exit(1);
 }
 
 octokit.authenticate({
   type: 'oauth',
-  token: secret,
+  token: secret || process.env.GITHUB_OAUTH_KEY,
 });
 
 if (star) {
   octokit.activity.starRepo({owner: 'Shastel', repo: 'runsubscribe'});
+  // eslint-disable-next-line
   console.log('Thanks =)');
-}
-
-function getProgressDefaults(total = 1) {
-  return {
-      complete: '=',
-      incomplete: ' ',
-      width: 20,
-      total,
-  };
 }
 
 const owner = 'rolling-scopes-school';
 
-async function paginate (method, options) {
-  const bar = new ProgressBar('Retrieving repos: [:bar] :percent :etas', getProgressDefaults());
+let unwatchQueue = [];
+const progress = new ProgressBar('Unwatching repos: [:bar] :percent :etas :current/:total', {
+    complete: '=',
+    incomplete: ' ',
+    width: 20,
+    total: 2,
+});
 
-  let response = await method(Object.assign({ per_page: 100 }, options));
-  let { data } = response;
+function processPartial(patial) {
+  const repos = patial.filter(({ name }) => name.indexOf(pattern) !== -1).map(({ name }) => name);
 
-  while (octokit.hasNextPage(response)) {
-    bar.total++;
-    response = await octokit.getNextPage(response);
-    bar.tick();
-    data = data.concat(response.data)
-  }
+  progress.tick(0, {
+    total: progress.total += repos.length,
+  });
 
-  bar.tick();
+  unwatchQueue = unwatchQueue.concat(repos);
 
-  return data;
+  return loadFromQueue();
 }
 
-paginate(octokit.repos.getForOrg, {
-  org: owner,
-  type: 'private'
-}).then((data) => {
-  return data.filter(({ name }) => name.indexOf(pattern) !== -1).map(({ name }) => name)
-}).then((names) => {
-  const bar = new ProgressBar('Unwatching repos: [:bar] :percent :etas', getProgressDefaults(names.length));
+async function loadFromQueue() {
+  if (!unwatchQueue.length) {
+    return;
+  }
 
-  names.reduce((R, repo) => {
-    return R.then(() => {
-      return octokit.activity.unwatchRepo({owner, repo}).then(() => {
-        bar.tick();
-      })
-    });
-  }, Promise.resolve());
-});
+  const repo = unwatchQueue.shift();
+
+  return octokit.activity.unwatchRepo({owner, repo})
+      .then(() => progress.tick())
+      // Each unwatch req will trigger 2 new, it significant faster, but don't trigger abuse mechanism
+      // In future can be uptadet to use 3 req at time
+      .then(() => Promise.all([ loadFromQueue(), loadFromQueue() ]));
+}
+
+async function loadRepos() {
+
+  let response = await octokit.repos.getForOrg(Object.assign({ per_page: 100 }, {
+    org: owner,
+    type: 'private'
+  }));
+
+  progress.tick();
+
+  const { data } = response;
+  processPartial(data)
+
+  while (octokit.hasNextPage(response)) {
+    progress.total++;
+    response = await octokit.getNextPage(response);
+    progress.tick();
+
+    processPartial(response.data);
+  }
+}
+
+loadRepos();
